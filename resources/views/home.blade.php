589764885734a2b11a4f73a78f1fac8768055486
DavidOlukunle <95961901+DavidOlukunle@users.noreply.github.com>
2023-02-16 10:03:32 +0100

    frontend changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
   <!-- ... -->
 </head>
 <body>
@@ -99,32 +100,88 @@
       <h3 class=" text-xm pt-3">We get insulted by others,</h3>
       <h3 class="text-xm ">loose trust for those we get back</h3>
     </div>
+  </div>
 
 
-{{-- 
-    <div class="grid grid-cols-1 gap-2  md:grid-cols-2 sm:grid-cols-2 pl-9 pt-9 auto">
-      <div class="pl-20 pt-7">
-        <h6 class="font-bold text-2xl">find the right doctor you need at your finger tip</h6>
-      </div> --}}
 
-      {{-- <div class="h-full">
-        <img src="image/doctor1.jpg"class="max-w-full h-56">
-          </div> --}}
+    <div class="grid grid-cols-1 pt-10 md:grid-cols-2 ">
+       <div class="h-full pl-20 pt-9 ">
+        <img src="image/doctor2.png"class="max-w-full h-62 w-3/4">
+          </div>
+      <div class="pr-20 pt-36">
+        <h3 class="text-blue-900 font-bold text-4xl">Find the right doctor right at your finger tip</h3><br>
+        <h3 class="text-gray-500 font-bold">We help you connect with the best doctors in the country. We are here to ensure that the best is given to our lovely patients. 
+          This is where the art of medicine is loved and  the love for humanity can not be overemphasized.
+        </h3>
+        <br><br>
+        <button type="button" class="w-32 py-2 px-5 bg-blue-600 text-white text-center align-center ">Read More</button>
+      </div> 
     </div>
 
 
+<div class="pt-20">
+  <h3 class="text-center text-3xl text-blue-700 font-bold">Our Team</h3>
+  <h3 class="text-center text-3xl text-blue-900 font-bold pt-4">Meet our specialists</h3>
+  <h2 class="text-center pt-5 pl-36 pr-36">Over the years, our team of specailists have grown both locally and internationally. We have ensured that the best of practioners are employed here with us. Not just that practioners who value life and hold humanity in high esteem</h2>
+</div>
 
 
 
+ <div id="dynamic-carousel" class="relative" data-carousel="static">
+  <!--carousel wrapper-->
+  <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+    <!--items to loop-->
+   
+    @foreach($docs as $doc)
+    <div class="hidden duration-700 ease-in-out pt-12 pl-400" data-carousel-item>
+     
+      <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg h-96 pl-40 shadow dark:bg-gray-800 dark:border-gray-700 align-center">
+        <a href="#">
+          <img src="{{ asset('storage/'.$doc->Image)}}" class="rounded-t-lg h-36" alt="" />
+        </a>
+        <div class="p-9">
+          <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$doc->name}}</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$doc->speciality}}</p>
+          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Read more
+              <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+      </div>
+  </div>
+      
+    </div>
+    @endforeach
+      <!-- Slider controls -->
+    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+      <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-red/30 dark:bg-red-800/30 group-hover:bg-red/50 dark:group-hover:bg-red-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-red-800/70 group-focus:outline-none">
+          <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+          <span class="sr-only">Previous</span>
+      </span>
+  </button>
+  <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+      <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+          <span class="sr-only">Next</span>
+      </span>
+  </button>
+
+  </div>
 
 
-{{-- 
-  @foreach($docs as $doc)
-
- <img src="{{$doc->Image ? asset('storage/'.$doc->Image): asset('/image/laravel.png')}}" width=500px>
  
+     
+     
 
-  @endforeach --}}
-
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+@foreach($docs as $doc)
+     
+     @endforeach
+</body>
+</html>
 
   
+
+
