e57b04c39295bc2b45588a1c2e3c4ba5ffc7feef
David Olukunle <olukunledavid24@gmail.com>
2026-09-22 00:42:33 +0100

    chore:clean production behaviour

--- docker/start.sh
+++ docker/start.sh
@@ -7,14 +7,6 @@ echo "Running Laravel migrations..."
 
 php artisan migrate --force
 
-echo "Seeding specialties..."
-
-php artisan db:seed --class=SpecialtySeeder --force
-
-echo "Seeding production admin..."
-
-php artisan db:seed --class=AdminSeeder --force
-
 echo "Starting Apache..."
 
 exec apache2-foreground
